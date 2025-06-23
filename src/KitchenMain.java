cc832f2f9b4959d8f01c345580cde7cbe7b3d743
Rikard <rikardengstrom89@gmail.com>
2025-06-23 19:31:35 +0200

    needs to revisit this tomorrow.

---
+++ src/KitchenMain.java
@@ -0,0 +1,18 @@
+public class KitchenMain {
+
+    public static void main(String[] args) {
+
+        SmartKitchen kitchen = new SmartKitchen();
+
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getBrewMaster().brewCoffee();
+//        kitchen.getIceBox().orderFood();
+
+        kitchen.setKitchenState(true, false, true);
+        kitchen.doKitchenWork();
+    }
+}
---
+++ src/SmartKitchen.java
@@ -0,0 +1,86 @@
+public class SmartKitchen {
+
+    private CoffeMaker brewMaster;
+    private Refrigerator iceBox;
+    private DishWasher dishWasher;
+
+    public SmartKitchen() {
+        brewMaster = new CoffeMaker();
+        iceBox = new Refrigerator();
+        dishWasher = new DishWasher();
+    }
+
+    public CoffeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public DishWasher getDishWasher() {
+        return dishWasher;
+    }
+
+    public void setKitchenState(boolean coffeFlag, boolean fridgeFlag, boolean dishWasherFlag) {
+
+        brewMaster.setHasWorkToDo(coffeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishWasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork() {
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishWasher.doDishes();
+    }
+
+}
+
+class Refrigerator  {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+    public void orderFood() {
+
+        if (hasWorkToDo) {
+            System.out.println("Ordering food....");
+            hasWorkToDo = false;
+        }
+    }
+        }
+
+        class DishWasher  {
+    private boolean hasWorkToDo;
+
+            public void setHasWorkToDo(boolean hasWorkToDo) {
+                this.hasWorkToDo = hasWorkToDo;
+            }
+
+            public void doDishes() {
+        if (hasWorkToDo) {
+            System.out.println("Starting the disher...");
+            hasWorkToDo = false;
+        }
+    }
+        }
+
+
+        class CoffeMaker  {
+    private boolean hasWorkToDo;
+
+            public void setHasWorkToDo(boolean hasWorkToDo) {
+                this.hasWorkToDo = hasWorkToDo;
+            }
+
+            public void brewCoffee() {
+                if (hasWorkToDo) {
+                    System.out.println("Starting coffee...");
+                    hasWorkToDo = false;
+                }
+            }
+        }
+
